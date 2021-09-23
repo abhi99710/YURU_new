0f045f3fa47d80622094576215f4b7539f94f480
aman9400 <amanshakya9400@gmail.com>
2021-09-24 01:58:54 +0530

    design added

---
+++ app/src/main/java/com/app/yuru/ui/transition/TransitionActivity.kt
@@ -0,0 +1,44 @@
+package com.app.yuru.ui.transition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.app.yuru.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class TransitionActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transition)
+
+        val firstFragment= TransitionToSleep()
+        val secondFragment=SleepEnhancer()
+        val thirdFragment=SleepEnhancer2()
+        val fourthFragment=SubliminalProgram()
+        val fifthFragment = WakeUpProgram()
+        setCurrentFragment(firstFragment)
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home_nv->setCurrentFragment(firstFragment)
+                R.id.journal_nv->setCurrentFragment(secondFragment)
+                R.id.wakeup_nv->setCurrentFragment(thirdFragment)
+                R.id.sleep_nv->setCurrentFragment(fourthFragment)
+                R.id.english_nv->setCurrentFragment(fifthFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.baseFrame,fragment)
+            commit()
+        }
+}
---
+++ app/src/main/java/com/app/yuru/ui/transition/TransitionToSleep.kt
@@ -0,0 +1,21 @@
+package com.app.yuru.ui.transition
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.yuru.R
+
+
+class TransitionToSleep : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transition_to_sleep, container, false)
+    }
+
+}
